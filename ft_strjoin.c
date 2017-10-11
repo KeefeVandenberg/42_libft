d38062a7ccea97737c95f2097b8ff7dd0f514a12
Keefe Vandenberg <kvandenb@e1z2r9p39.42.us.org>
2017-10-11 15:38:19 -0700

    Pushing\ the\ libft

---
+++ ft_strjoin.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvandenb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/29 18:53:21 by kvandenb          #+#    #+#             */
+/*   Updated: 2017/10/05 18:17:26 by kvandenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *new;
+
+	if (!s1 || !s2)
+		return (NULL);
+	new = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (!new)
+		return (NULL);
+	ft_strncpy(new, s1, ft_strlen(s1));
+	ft_strncat(new, s2, ft_strlen(s2));
+	return (new);
+}
---
+++ ft_strsub.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvandenb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/29 14:39:56 by kvandenb          #+#    #+#             */
+/*   Updated: 2017/10/05 15:33:14 by kvandenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	if (!(s))
+		return (NULL);
+	str = ft_strnew(len);
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	return (str);
+}
